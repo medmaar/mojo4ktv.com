ae2aed331db7aa1b55d2b22611f2449dc5fe142c
Mo <mo@iptv.com>
2026-09-03 22:04:08 +0000

    Update server URL to line.truthdaily.me

--- worker/worker.js
+++ worker/worker.js
@@ -6,7 +6,7 @@
 
 const API_BASE    = "https://activationpanel.ru/api/api.php";
 const API_KEY     = "35cf68cc83a3a82e1a0ac5361c7b6105";
-const HOST        = "http://mag.trexlive.me";
+const HOST        = "http://line.truthdaily.me";
 const FROM_EMAIL  = "Mojo 4K TV <help@mojo4ktv.com>";
 const ADMIN_EMAIL = "help@mojo4ktv.com";
 const SITE_URL    = "https://mojo4ktv.com";
